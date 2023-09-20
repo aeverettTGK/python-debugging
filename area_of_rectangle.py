4049ad936bbb3880aeaabe7960d391220d416c4f
Alex Everett <aeverett4746@gmail.com>
2023-09-20 14:54:49 -0600

    Debugged area_of_rectangle.py

--- area_of_rectangle.py
+++ area_of_rectangle.py
@@ -11,10 +11,10 @@ def area_of_rectangle(height, width = None):
 
     Parameters
     ----------
-    height : int or float 
+    height : int or float
         The height of the rectangle.
     width : int or float
-        The width of the rectangle. If `None` width is assumed to be equal to 
+        The width of the rectangle. If `None` width is assumed to be equal to
         the height.
 
     Returns
@@ -29,9 +29,9 @@ def area_of_rectangle(height, width = None):
     >>> area_of_rectangle (7, 2)
     14
     """
-    if width:
+    if width == None:
         width = height
-    area = height * width
+    area = int(height) * int(width)
     return area
 
 if __name__ == '__main__':
@@ -43,8 +43,8 @@ if __name__ == '__main__':
         sys.exit(message)
     height = sys.argv[1]
     width = height
-    if len(sys.argv) > 3:
-        width = sys.argv[1]
+    if len(sys.argv) >= 3:
+        width = sys.argv[2]
 
     area = area_of_rectangle(height, width)
 
